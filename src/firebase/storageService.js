cdd0a2cc3ef5fc68104ae3941f1c43681086cd5e
Kabir Alkasim <codesmartng1@gmail.com>
2026-08-26 18:26:29 +0100

    Remove updateUserProfile function
    Removed the updateUserProfile function from storageService.js.

--- src/firebase/storageService.js
+++ src/firebase/storageService.js
@@ -780,23 +780,6 @@ export const subscribeToCourse = (courseId, callback) => {
   return unsubscribe;
 };
 
-// ==================== USER PROFILE ====================
-
-export const updateUserProfile = async (uid, profileData) => {
-  try {
-    const userRef = doc(db, 'users', uid);
-
-    await updateDoc(userRef, {
-      ...profileData,
-      updatedAt: serverTimestamp()
-    });
-
-    return true;
-  } catch (error) {
-    console.error('Error updating user profile:', error);
-    throw error;
-  }
-};
 
 // ==================== EXPORTS ====================
 
